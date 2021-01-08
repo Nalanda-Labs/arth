f8b96bd61c99d7a8de2dc31cab68155631ff0fe7
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2021-01-08 11:32:15 +0530

    basic config file

--- backend/src/main.cc
+++ backend/src/main.cc
@@ -4,13 +4,6 @@ using namespace drogon;
 
 int main() {
     std::cout << banner << std::endl;
-    // app().addListener("::1", 8848); //ipv6
-    app().addListener("0.0.0.0", 8000);
-    if (app().supportSSL())
-    {
-        drogon::app()
-            .setSSLFiles("server.pem", "server.pem")
-            .addListener("0.0.0.0", 8849, true);
-    }
+    app().loadConfigFile("config.json");
     app().run();
 }
