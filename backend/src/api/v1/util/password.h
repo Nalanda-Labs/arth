654afe8c65db18fa9cb71c6140f059b6856ccf5f
Hemil Ruparel <hemilruparel2002@gmail.com>
2021-01-10 12:23:23 +0530

    Added dummy password hashing and verify functions

---
+++ backend/src/api/v1/util/password.h
@@ -0,0 +1,10 @@
+
+
+inline std::string hash_password(const std::string &password) {
+  return password;
+}
+
+
+inline bool verify_password(const std::string &password, const std::string &hash) {
+  return hash_password(password) == hash;
+}
