5b15ea2ad454d43a51a0898a295055601bfcca96
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2021-01-10 20:59:08 +0530

    renamed password.h

--- backend/src/api/v1/util/password.h
+++
@@ -1,8 +0,0 @@
-inline std::string hash_password(const std::string &password) {
-  return password;
-}
-
-
-inline bool verify_password(const std::string &password, const std::string &hash) {
-  return hash_password(password) == hash;
-}
