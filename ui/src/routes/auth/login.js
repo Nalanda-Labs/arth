1e2530fc641618cc470e5372b71b03c9622681a1
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2021-01-12 20:49:54 +0530

    bugfix when login does not return jwt

--- ui/src/routes/auth/login.js
+++ ui/src/routes/auth/login.js
@@ -4,7 +4,10 @@ import jwt_decode from "jwt-decode";
 export function post(req, res) {
 
     api.post('login', req.body).then(response => {
-        let decoded = jwt_decode(response.jwt);
+        let decoded = {};
+        if (response.jwt) {
+            decoded = jwt_decode(response.jwt);
+        }
 
         if (decoded.username) req.session.user = decoded.username;
         res.setHeader('Content-Type', 'application/json');
