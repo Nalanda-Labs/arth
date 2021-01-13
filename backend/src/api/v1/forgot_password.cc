f27b63194994f9b037d9aca288494b0d3a31b1dd
Hemil Ruparel <hemilruparel2002@gmail.com>
2021-01-13 19:56:12 +0530

    Added changePassword
    Some formatting changes in forgot_password.cc

--- backend/src/api/v1/forgot_password.cc
+++ backend/src/api/v1/forgot_password.cc
@@ -10,6 +10,7 @@
 #include "util/datetime.h"
 #include "util/emailutils.h"
 #include "util/input_validation.h"
+#include "util/password.h"
 
 #include "plugins/email.h"
 
@@ -81,90 +82,198 @@ void ForgotPassword::forgotPassword(const HttpRequestPtr &req,
     {
         auto clientPtr = drogon::app().getFastDbClient("default");
         clientPtr->newTransactionAsync([=](TransactionPtr transactionPtr) mutable {
-        assert(transactionPtr);
-        auto customConfig = app().getCustomConfig();
+            assert(transactionPtr);
+            auto customConfig = app().getCustomConfig();
 
-        transactionPtr->execSqlAsync(
-            "select id, email_verified from users where email = $1 or username = "
-            "$2",
-            [=](const Result &r) mutable {
-                if (r.size() != 1) {
-                    /// **Important**: Do not let the UI know that user does not
-                    /// exist. It can be misused by competitors or hackers to know who
-                    /// all are affiliated with us.
-                    ret["message"] = "Email has been sent to " + email;
-
-                    auto smtp = SMTPMail();
-                    auto base_url = customConfig.get("base_url", "").asString();
-                    smtp.sendEMail(
-                        email, 
-                        "Password recovery for Arth",
-                        std::string("Someone tried to reset password on ") + base_url + ". "
-                        "If you find this error please ignore this email.",
-                        customConfig
-                    );
-
-                    LOG_WARN << "Non existant username or email requested for "
-                                "password recovery";
-
-                    callback(jsonResponse(std::move(ret)));
-                    return;
-                }
-
-                auto row = r[0];
-
-                auto email_verified = row["email_verified"].as<bool>();
-
-                if (!email_verified) {
-                    /// C and C++ concatenate such char* automatically.
-                    /// This is const char * not std::string. std::string
-                    /// is concatenated using operator +
-                    ret["error"] =
-                        "Email not verified. You need to "
-                        "verify your email before trying to reset password";
-                    callback(jsonResponse(std::move(ret)));
-                    return;
-                }
-
-                auto user_id = row["id"].as<size_t>();
-
-                std::string token = uuid();
-
-                std::string createdAt = DateTime::getLocalDateTimeISOFormat();
-
-                transactionPtr->execSqlAsync(
-                    "insert into email_tokens (user_id, email, token, created_at, "
-                    "updated_at) "
-                    "values ($1, $2, $3, $4, $5)",
-
-                    [=](const Result &r) mutable {
-                        auto smtp = SMTPMail();
-                        auto base_url = customConfig["base_url"].asString();
-                        smtp.sendEMail(email, "Password recovery for Arth",
-                                        std::string("Click on the link <a href=\"") +
-                                            base_url +
-                                            "/verify-forgot-password?token=" + token +
-                                            "\"> " + "to change your password.",
-                                        customConfig);
-
+            transactionPtr->execSqlAsync(
+                "select id, email_verified from users where email = $1 or username = "
+                "$2",
+                [=](const Result &r) mutable {
+                    if (r.size() != 1) {
+                        /// **Important**: Do not let the UI know that user does not
+                        /// exist. It can be misused by competitors or hackers to know who
+                        /// all are affiliated with us.
                         ret["message"] = "Email has been sent to " + email;
-                        callback(jsonResponse(std::move(ret)));
-                    },
 
-                    [=](const DrogonDbException &e) mutable {
-                        LOG_DEBUG << e.base().what();
-                        ret["error"] = (std::string)e.base().what();
-                        callback(jsonResponse(std::move(ret)));
-                    },
+                        auto smtp = SMTPMail();
+                        auto base_url = customConfig.get("base_url", "").asString();
+                        smtp.sendEMail(
+                            email, 
+                            "Password recovery for Arth",
+                            std::string("Someone tried to reset password on ") + base_url + ". "
+                            "If you find this error please ignore this email.",
+                            customConfig
+                        );
 
-                    user_id, email, token, createdAt, createdAt);
-            },
-            [=](const DrogonDbException &e) mutable {
-                LOG_DEBUG << e.base().what();
-                ret["error"] = (std::string)e.base().what();
-                callback(jsonResponse(std::move(ret)));
-            },
-            email, username);
+                        LOG_WARN << "Non existant username or email requested for "
+                                    "password recovery";
+
+                        callback(jsonResponse(std::move(ret)));
+                        return;
+                    }
+
+                    auto row = r[0];
+
+                    auto email_verified = row["email_verified"].as<bool>();
+
+                    if (!email_verified) {
+                        /// C and C++ concatenate such char* automatically.
+                        /// This is const char * not std::string. std::string
+                        /// is concatenated using operator +
+                        ret["error"] =
+                            "Email not verified. You need to "
+                            "veqrify your email before trying to reset password";
+                        callback(jsonResponse(std::move(ret)));
+                        return;
+                    }
+
+                    auto user_id = row["id"].as<size_t>();
+
+                    std::string token = uuid();
+
+                    std::string createdAt = DateTime::getLocalDateTimeISOFormat();
+
+                    /// Token is unique indexed in the database. 
+                    /// If uuid collides (highly highly unlikely), 
+                    /// then insert will fail and user will get an error. 
+                    /// They will try again and succeed (mostly).                    
+                    transactionPtr->execSqlAsync(
+                        "insert into email_tokens (user_id, email, token, created_at, "
+                        "updated_at) "
+                        "values ($1, $2, $3, $4, $5)",
+
+                        [=](const Result &r) mutable {
+                            auto smtp = SMTPMail();
+                            auto base_url = customConfig["base_url"].asString();
+                            smtp.sendEMail(email, "Password recovery for Arth",
+                                            std::string("Click on the link <a href=\"") +
+                                                base_url +
+                                                "/verify-forgot-password?token=" + token +
+                                                "\"> " + "to change your password.",
+                                            customConfig);
+
+                            ret["message"] = "Email has been sent to " + email;
+                            callback(jsonResponse(std::move(ret)));
+                        },
+
+                        [=](const DrogonDbException &e) mutable {
+                            LOG_DEBUG << e.base().what();
+                            ret["error"] = (std::string)e.base().what();
+                            callback(jsonResponse(std::move(ret)));
+                        },
+
+                        user_id, email, token, createdAt, createdAt
+                    );
+                },
+                [=](const DrogonDbException &e) mutable {
+                    LOG_DEBUG << e.base().what();
+                    ret["error"] = (std::string)e.base().what();
+                    callback(jsonResponse(std::move(ret)));
+                },
+                email, username
+            );
         });
     }
+}
+
+
+void ForgotPassword::changePassword(const HttpRequestPtr &req, Callback callback) {
+    auto json = req->getJsonObject();
+    Json::Value ret;
+
+    if (json == nullptr) {
+        LOG_DEBUG << "Invalid input";
+        ret["error"] = "Invalid input";
+        callback(jsonResponse(std::move(ret)));
+        return;
+    }
+
+    auto token = req->getParameter("token");
+    auto newPassword = json->get("new_password", "").asString();    
+
+    LOG_DEBUG << "token: " << token << "\n";
+    LOG_DEBUG << "new password: " << newPassword;    
+
+
+    if (token == "" || newPassword == "") {
+        LOG_DEBUG << "Invalid input";
+        ret["error"] = "Invalid input";
+        callback(jsonResponse(std::move(ret)));
+        return;
+    }
+
+    if (!isPasswordValid(newPassword)) {
+        LOG_DEBUG << "Invalid password";
+        ret["error"] = "Password should be at least 16 and 256 characters";
+        callback(jsonResponse(std::move(ret)));
+        return;
+    }
+
+    {
+        auto clientPtr = app().getFastDbClient("default");
+        clientPtr->newTransactionAsync([=](TransactionPtr transactionPtr) mutable {
+            
+            auto now = DateTime::getLocalDateTimeISOFormat();
+
+            transactionPtr->execSqlAsync(
+                "update email_tokens "                
+                "set confirmed = true, updated_at = $1"
+                "where expired = false "
+                "and confirmed = false "
+                "and token = $2 "
+                "returning user_id", 
+                [=] (const Result &r) mutable {
+
+                    /// Token is unique. inserting a token that already exists wont happen.
+                    /// So we dont need to check if more than one row is affected
+                    if (r.size() != 1) {
+                        LOG_DEBUG << "Token doesn't exist or is expired. Please try resetting password again";
+                        ret["error"] = "Token doesn't exist or is expired. Please try resetting password again";
+                        callback(jsonResponse(std::move(ret)));
+                        return;
+                    }       
+
+                    auto [passwordHash, salt] = PasswordUtils::hashPassword(newPassword, "");
+
+                    LOG_DEBUG << "password_hash: " << passwordHash;
+                    LOG_DEBUG << "salt: " << salt;
+
+                    if (passwordHash.empty() || salt.empty()) {
+                        LOG_ERROR << "Error hashing password";
+                        ret["error"] = "An error occurred. Please contact support.";
+                        callback(HttpResponse::newHttpJsonResponse(std::move(ret)));
+                        return;
+                    }             
+
+                    /// we have already checked if one row is returned
+                    auto userID = r[0]["user_id"].as<size_t>();
+
+                    transactionPtr->execSqlAsync(
+                        "update users "
+                        "set password_hash = $1, salt = $2 "
+                        "where id = $3 ",
+                        [=] (const Result &r) mutable {
+                            LOG_DEBUG << "Password updated";
+                            ret["message"] = "Password updated";
+                            callback(jsonResponse(std::move(ret)));
+                            return;
+
+                        },
+                        [=](const DrogonDbException &e) mutable {
+                            LOG_DEBUG << e.base().what();
+                            ret["error"] = (std::string)e.base().what();
+                            callback(jsonResponse(std::move(ret)));
+                        },
+                        passwordHash, salt, userID
+                    );
+                },
+                [=](const DrogonDbException &e) mutable {
+                    LOG_DEBUG << e.base().what();
+                    ret["error"] = (std::string)e.base().what();
+                    callback(jsonResponse(std::move(ret)));
+                },
+                now, token
+            );
+        });
+    }    
 }
