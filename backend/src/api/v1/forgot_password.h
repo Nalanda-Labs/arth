b27282e8ea4ea268dc90054268b96b6ab32d4d1a
Hemil Ruparel <hemilruparel2002@gmail.com>
2021-01-14 13:02:26 +0530

    use path parameters

--- backend/src/api/v1/forgot_password.h
+++ backend/src/api/v1/forgot_password.h
@@ -22,10 +22,10 @@ public:
     METHOD_LIST_BEGIN
     // use METHOD_ADD to add your custom processing function here;
     ADD_METHOD_TO(ForgotPassword::forgotPassword, "/api/v1/forgot-password", Post, Options);
-    ADD_METHOD_TO(ForgotPassword::changePassword, "/api/v1/change-password", Post, Options);
+    ADD_METHOD_TO(ForgotPassword::changePassword, "/api/v1/change-password?token={string token}", Post, Options);
     METHOD_LIST_END
 
     void forgotPassword(const HttpRequestPtr &req, Callback callback);
-    void changePassword(const HttpRequestPtr &req, Callback callback);
+    void changePassword(const HttpRequestPtr &req, Callback callback, const std::string &token);
 };
 } // namespace api::v1
