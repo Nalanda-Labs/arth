86ffb8478f56b25ae7e10010b22572353526f5d0
Hemil Ruparel <hemilruparel2002@gmail.com>
2021-01-15 17:15:23 +0530

    initial updateProfle (seg faults)

--- backend/src/api/v1/profile.cc
+++ backend/src/api/v1/profile.cc
@@ -1,8 +1,11 @@
+#include <cstddef>
 #include <drogon/HttpAppFramework.h>
+#include <drogon/orm/Exception.h>
 #include <trantor/utils/Logger.h>
 
 #include "profile.h"
 #include "util/arth.h"
+#include "util/emailutils.h"
 #include "util/input_validation.h"
 
 using namespace drogon;
@@ -66,4 +69,184 @@ void profile::getProfile(const HttpRequestPtr &req, Callback callback, std::stri
             username
         );
     }    
+}
+
+
+struct User {
+    const size_t id;
+    const std::string name, title, designation, email;
+
+    User(const size_t id, const std::string name, const std::string title, const std::string designation, const std::string email) 
+    : id(id), name(name), title(title), designation(designation), email(email) {}
+};
+
+
+using DbResultCallback = std::function<void(const Result &)>;
+
+/**
+ * @brief Update user with old username to new username and using other details from user
+ * 
+ * @param transPtr transaction pointer to use
+ * @param newUsername new username
+ * @param oldUsername old username
+ * @param user updated values of user
+ * @param resultCallback callback containing result of update if update was successful
+ * @param exceptionCallback callback returning the exeption if update failed
+ */
+void updateUser(
+    TransactionPtr transPtr, 
+    const std::string newUsername, 
+    const std::string oldUsername,
+    const User user,
+    DbResultCallback resultCallback,
+    DrogonDbExceptionCallback exceptionCallback);
+
+
+void profile :: updateProfile(const HttpRequestPtr &req, Callback callback, std::string &&username) {
+    auto json = req->getJsonObject();
+
+    Json::Value ret;
+    if (json == nullptr) {
+        ret["error"] = "Malformed request";
+        callback(jsonResponse(std::move(ret)));
+        return;
+    }
+
+    auto newUsername = json->get("newUsername", "").asString();
+    auto name = json->get("name", "").asString();
+    auto title = json->get("title", "").asString();
+    auto designation = json->get("designation", "").asString();
+    auto email = json->get("email", "").asString();
+
+    LOG_DEBUG << "new username: " << newUsername;
+    LOG_DEBUG << "name: " << name;
+    LOG_DEBUG << "title: " << title;
+    LOG_DEBUG << "designation: " << designation;
+    LOG_DEBUG << "email: " << email;
+    EmailUtils::cleanEmail(email);
+    LOG_DEBUG << "cleaned email: " << email;
+
+    if (username.empty() || newUsername.empty() || name.empty() || title.empty() || designation.empty() || email.empty()) {
+        ret["error"] = "Some or all of the parameters are empty";
+        callback(jsonResponse(std::move(ret)));
+        return;
+    }    
+
+    if (!isUsernameValid(username) || !isUsernameValid(newUsername) || !isEmailValid(email)) {
+        ret["error"] = "Invalid input";
+        callback(jsonResponse(std::move(ret)));
+        return;
+    }
+
+    auto customConfig = app().getCustomConfig();
+    auto jwt_secret = customConfig.get("jwt_secret", "").asString();
+
+    if (jwt_secret == "") {
+        ret["error"] = "JWT not configured";
+        callback(jsonResponse(std::move(ret)));
+        return;        
+    }
+
+    auto optionalToken = verifiedToken(req->getHeader("Authorization"), jwt_secret);
+
+    if (!optionalToken.has_value()) {
+        ret["error"] = "Authentication Error";
+        callback(jsonResponse(std::move(ret)));
+        return;
+    }
+
+    auto token = optionalToken.value();
+    if (token.username != username) {
+        ret["error"] = "Authentication Error";
+        callback(jsonResponse(std::move(ret)));
+        return;
+    }
+
+    {
+        auto clientPtr = app().getFastDbClient("default");
+        clientPtr->newTransactionAsync([=](TransactionPtr transPtr) mutable {
+            
+            User user{token.userID, name, title, designation, email};
+
+            auto updateUserAndReturnJson = [=]() mutable {
+                updateUser(
+                    transPtr, 
+                    newUsername, 
+                    username, 
+                    user, 
+                    [=](const Result & r) mutable {
+                        LOG_DEBUG << "in result callback";
+                        ret["message"] = "Changes saved";
+                        callback(jsonResponse(std::move(ret)));        
+                    }, 
+                    [=] (const DrogonDbException & e) mutable {
+                        LOG_DEBUG << e.base().what();
+                        ret["error"] = (std::string)e.base().what();
+                        callback(HttpResponse::newHttpJsonResponse(std::move(ret)));
+                    }
+                );
+            };
+
+            if (username != newUsername) {
+                transPtr->execSqlAsync(
+                    "select id from users where username = $1", 
+                    [=] (const Result &r) mutable {
+                        if (!r.empty() && newUsername != username) {
+                            ret["error"] = "Username is already taken";
+                            callback(jsonResponse(std::move(ret)));
+                            return;
+                        }
+                        updateUserAndReturnJson();
+                    },
+                    [=](const DrogonDbException &e) mutable {
+                        LOG_DEBUG << e.base().what();
+                        ret["error"] = (std::string)e.base().what();
+                        callback(HttpResponse::newHttpJsonResponse(std::move(ret)));
+                    },
+                    newUsername
+                );
+            } else {
+                updateUserAndReturnJson();
+            }
+        });
+    }
+}
+
+
+/**
+ * @brief Update user with old username to new username and using other details from user
+ * 
+ * @param transPtr transaction pointer to use
+ * @param newUsername new username
+ * @param oldUsername old username
+ * @param user updated values of user
+ * @param resultCallback callback containing result of update if update was successful
+ * @param exceptionCallback callback returning the exeption if update failed
+ */
+void updateUser(
+    TransactionPtr transPtr, 
+    const std::string newUsername, 
+    const std::string oldUsername,
+    const User user,
+    DbResultCallback resultCallback,
+    DrogonDbExceptionCallback exceptionCallback) {
+
+    auto usernameLower = newUsername;
+
+    transform(usernameLower.begin(), usernameLower.end(), usernameLower.begin(), ::tolower);
+    LOG_DEBUG << "username lower: " << usernameLower;
+
+    transPtr->execSqlAsync(
+        "update users "
+        "set username = $1, "
+        "username_lower = $2, "
+        "name = $3, "
+        "title = $4, "
+        "designation = $5, "
+        "email = $6 "
+        "where id = $7",         
+        resultCallback,
+        exceptionCallback,
+        newUsername, usernameLower, user.name, user.title, user.designation, user.email, user.id
+    );
 }
--- backend/src/api/v1/profile.h
+++ backend/src/api/v1/profile.h
@@ -9,12 +9,12 @@ namespace api::v1 {
 class profile:public drogon::HttpController<profile>
 {
   public:    
-    METHOD_LIST_BEGIN    
-    /// Post because user may require authentication. 
-    /// Get requests with bodies are not standard.
+    METHOD_LIST_BEGIN        
     ADD_METHOD_TO(profile ::getProfile, "/api/v1/profile/{string username}", Get, Options); 
+    ADD_METHOD_TO(profile ::updateProfile, "/api/v1/profile/{string username}", Put);
     METHOD_LIST_END
 
     void getProfile(const HttpRequestPtr &req, Callback callback, std::string &&username);
+    void updateProfile(const HttpRequestPtr &req, Callback callback, std::string &&username);
 };
 }//namespace api::v1
